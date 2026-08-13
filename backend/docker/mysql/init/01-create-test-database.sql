ecbe3818a24d52d9ffe8e478279f96342ea9d4f0
m9020120014 <m9020120014@gmail.com>
2026-08-13 05:53:50 +0200

    chore: remove backend implementation and related files for event care API

--- backend/docker/mysql/init/01-create-test-database.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE IF NOT EXISTS tournament_test
-    CHARACTER SET utf8mb4
-    COLLATE utf8mb4_unicode_ci;
-
-GRANT ALL PRIVILEGES ON tournament_test.* TO 'tournament'@'%';
-
-FLUSH PRIVILEGES;
--- mobile/android/app/src/main/kotlin/com/prowem/prowem_live_score/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.prowem.prowem_live_score
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
